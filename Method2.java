b214af0b585f7337640086ad90dd3bb494df72d9
nadnadia25 <55502535+nadnadia25@users.noreply.github.com>
2020-04-29 12:24:44 +0800

    Add files via upload

---
+++ Method2.java
@@ -0,0 +1,16 @@
+package com.interface1;
+
+public class Method2 {
+
+    public static void main(String[] args) {
+
+        MyInterface myInterface= new MyInterface() {
+            @Override
+            public void sayHello() {
+                System.out.println("Hello Method2");
+            }
+        };
+        myInterface.sayHello();
+    }
+}
+
---
+++ MyInterface.java
@@ -0,0 +1,6 @@
+package com.interface1;
+
+public interface MyInterface {
+
+    void sayHello();
+}
